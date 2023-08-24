e0b50d8a06739668459657b20d187576499f6462
Hadra 92i <hadraiizi547@gmail.com>
2023-08-24 23:37:40 +0000

    crud drivers

---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 fw-bold">
+                <script>document.write(new Date().getFullYear())</script> &copy; Kalini Taxi
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,58 @@
+<div class="leftside-menu">
+
+    <!-- LOGO -->
+    <a href="#" class="logo text-center logo-light">
+                    <span class="logo-lg">
+                        <img src="{{asset('assets/images/logo.png')}}" alt="" height="16">
+                    </span>
+        <span class="logo-sm">
+                        <img src="{{asset('assets/images/logo_sm.png')}}" alt="" height="16">
+                    </span>
+    </a>
+
+    <!-- LOGO -->
+    <a href="#" class="logo text-center logo-dark">
+                    <span class="logo-lg">
+                        <img src="{{asset('assets/images/logo-dark.png')}}" alt="" height="16">
+                    </span>
+        <span class="logo-sm">
+                        <img src="{{asset('assets/images/logo_sm_dark.png')}}" alt="" height="16">
+                    </span>
+    </a>
+
+    <div class="h-100" id="leftside-menu-container" data-simplebar="">
+
+        <!--- Sidemenu -->
+        <ul class="side-nav">
+
+            <li class="side-nav-title side-nav-item">Navigation</li>
+
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarDashboards" aria-expanded="false" aria-controls="sidebarDashboards" class="side-nav-link">
+                    <i class="uil-home-alt"></i>
+                    <span class="badge bg-success float-end">4</span>
+                    <span> Dashboards </span>
+                </a>
+                <div class="collapse" id="sidebarDashboards">
+                    <ul class="side-nav-second-level">
+
+                    </ul>
+                </div>
+            </li>
+
+            <li>
+                <a href="{{route('admin.drivers')}}" class="side-nav-link text-white">
+                    <i class="mdi mdi-account-multiple"></i>
+                    <span>Chauffeurs</span>
+                </a>
+            </li>
+        </ul>
+
+        <!-- End Sidebar -->
+
+        <div class="clearfix"></div>
+
+    </div>
+    <!-- Sidebar -left -->
+
+</div>
